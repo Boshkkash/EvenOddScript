2199e04d14e78f2db4276dff2013e2d2fa4ee96a
Omar Galal <125661136+omargalal232@users.noreply.github.com>
2023-08-23 14:22:16 +0300

    Added the even_odd code, checks if the user input is even or odd, then displays a message indicating it. Contributor: Omar Walid Galal (#3)
    * Created even_odd code by OmarWalidGalal
    
    * Added my info (name,,email,phone)

---
+++ even_odd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter a number:"
+read number
+
+if ((number % 2 == 0)); then
+    echo "$number is even."
+else
+    echo "$number is odd."
+fi
+
+# Omar Walid Galal
+# omargalal232@gmail.com
+# 01225663158
